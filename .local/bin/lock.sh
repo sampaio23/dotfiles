ae084dfc730b10c6d4fa4793deabf0889958acef
sampaio23 <sampaio.ime@gmail.com>
2020-03-08 00:45:15 -0300

    lock working now after kibada

--- .local/bin/lock.sh
+++ .local/bin/lock.sh
@@ -22,7 +22,9 @@ font="sans-serif"
 
 mpc pause
 
-i3lock -t -i $(ls $HOME/.backgrounds | sort -R | tail -1) \
+img=$(ls /home/luiz-sampaio/.backgrounds | sort -R | tail -1 | tr -d '%\n')
+
+i3lock -t -i $HOME/.backgrounds/$img \
 	--timepos='x+110:h-70' \
 	--datepos='x+43:h-45' \
 	--clock --date-align 1 --datestr "$locktext" \
