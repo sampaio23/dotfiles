4fa1e2b0decda1e1b32425285033bfc414f4901c
Luiz Sampaio <sampaio.ime@gmail.com>
2026-06-06 20:56:43 +0100

    neovim: don't highlight on yanking

--- neovim/.config/nvim/lua/core/autocmds.lua
+++ neovim/.config/nvim/lua/core/autocmds.lua
@@ -1,13 +1,5 @@
 local augroup = vim.api.nvim_create_augroup("core", { clear = true })
 
-vim.api.nvim_create_autocmd("TextYankPost", {
-  group = augroup,
-  desc = "Highlight yanked text",
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
-
 vim.api.nvim_create_autocmd("BufReadPost", {
   group = augroup,
   desc = "Restore cursor position",
